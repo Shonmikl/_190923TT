8f6ad1f484a0c409c4d09c2134eb8dfdf4a914db
shonmikl <mikhailegorov676@gmail.com>
2024-01-16 11:32:12 +0100

    taski day

---
+++ src/main/java/org/example/_2024_01_16/LNode.java
@@ -0,0 +1,27 @@
+package org.example._2024_01_16;
+
+public class LNode {
+    int x;
+    LNode next;
+
+    public LNode(int x) {
+        this.x = x;
+        this.next = null;
+    }
+
+    public static void main(String[] args) {
+        LNode n1 = new LNode(1);
+        LNode n2 = new LNode(2);
+        LNode n3 = new LNode(3);
+        LNode n4 = new LNode(4);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n2;
+    }
+
+    public static boolean isC(LNode head) {
+        return true;
+    }
+}

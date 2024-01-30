7a4e12a8d76dcf4724ddaee071c1bf9f1a0b7e90
shonmikl <mikhailegorov676@gmail.com>
2024-01-30 10:07:06 +0100

    IO

---
+++ src/main/java/org/example/_2024_01_30/PathExample.java
@@ -0,0 +1,17 @@
+package org.example._2024_01_30;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathExample {
+    public static void main(String[] args) {
+        Path path = Paths.get("prog.txt");
+
+//        System.out.println(path.getFileName());
+//        System.out.println(path.getParent());
+//        System.out.println(path.getRoot());
+
+//        Path subpath = path.subpath(1, 2);
+//        System.out.println(subpath);
+    }
+}

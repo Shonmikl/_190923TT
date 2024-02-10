f08b127307beae2694b45846e1a7ac848684c06a
shonmikl <mikhailegorov676@gmail.com>
2024-02-10 14:37:36 +0100

    dry

---
+++ src/main/java/org/example/Parent.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.List;
+
+public abstract class Parent {
+    /**
+     *
+     *
+     * an = 1 + (-1)n+1   / n
+     */
+    static double seq(int n) {
+        return (1 + Math.pow(-1, n+1)) / n;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.print(seq(i) + ", ");
+        }
+    }
+}
+
+
